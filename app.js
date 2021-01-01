0d1dc2e27752e219a2e26ccf5e145fc573ce5d47
rapyon9 <64957267+rapyon9@users.noreply.github.com>
2021-01-01 23:30:52 +0900

    #2.1 Drawing on Canvas

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const canvas = document.getElementById("jsCanvas");
+canvas.height = canvas.offsetHeight;
+canvas.width = canvas.offsetWidth;
+
+const ctx = canvas.getContext("2d");
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
 
 let painting = false;
+
+
+function startPainting(){
+    painting = true;
+}
 
 function stopPainting(){
     painting = false;
@@ -9,24 +21,25 @@ function stopPainting(){
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
-function onMouseDown(event){
-    painting = true;
-}
-
-function onMouseUp(event){
-    stopPainting();
-}
 
 //캔버스 위에서
 if(canvas){
     //마우스를 올려다놓을때
     canvas.addEventListener("mousemove", onMouseMove);
     //마우스를 클릭해서 선을 그릴때
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
     //마우스 클릭을 멈췄을때
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseup", stopPainting);
     //마우스가 캔버스 밖으로 나갔을때
     canvas.addEventListener("mouseleave", stopPainting);
 }
